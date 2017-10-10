fae90cf1411169169f1ab572df1e948270e775bb
Rohan Weeden <askaholic907@gmail.com>
2017-10-10 11:14:44 -0800

    Need to fix max

--- contigsum/contigsum.hpp
+++ contigsum/contigsum.hpp
@@ -6,17 +6,37 @@
 #define __con_ti_g_SUM__h_PP___
 
 #include <tuple>
+#include <algorithm>
 
 template<typename RAIter>
 std::tuple<int, int, int, int> contigSumRec(RAIter first, RAIter last) {
-  int length = std::distance(first, last);
+  auto length = std::distance(first, last);
 
   if (length == 1) {
-    int gcs = std::max(0, *first);
+    auto gcs = std::max(0, *first);
     return std::make_tuple(gcs, gcs, gcs, *first);
   }
 
-  return std::make_tuple(0, 0, 0, 0);
+  auto mid = first;
+  std::advance(mid, length / 2);
+
+  auto first_half = contigSumRec(first, mid);
+  auto second_half = contigSumRec(mid, last);
+
+  auto start_gcs = 0;
+  auto end_gcs = 0;
+
+  if (std::get<1>(first_half) == std::get<3>(first_half))
+      start_gcs = std::get<1>(first_half) + std::get<1>(second_half);
+  else
+      start_gcs = std::max(std::get<1>(first_half), std::get<3>(first_half) + std::get<1>(second_half));
+
+  if (std::get<2>(second_half) == std::get<3>(second_half))
+      end_gcs = std::get<2>(first_half) + std::get<2>(second_half);
+  else:
+      end_gcs = std::max(std::get<2>(second_half), std::get<3>(second_half) + std::get<2>(first_half));
+
+  return std::make_tuple(std::max(std::get<0>(first_half), std::get<2>(second_half), std::get<2>(first_half) + std::get<1>(second_half)), start_gcs, end_gcs, std::get<3>(first_half) + std::get<3>(second_half));
 }
 
 template<typename RAIter>

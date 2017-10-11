f5f8e04ab03acd9ff222e83d75b071e0167e5b5d
Rohan Weeden <askaholic907@gmail.com>
2017-10-11 09:31:09 -0800

    Added inversions template. Don't know where the sorting code is though

---
+++ inversions/inversions.hpp
@@ -0,0 +1,8 @@
+// inversions.hpp
+// Rohan Weeden
+// Created: October 11, 2017
+
+// Count the number of inversions in a range
+
+template<typename RAIter>
+size_t inversions(RAIter first, RAIter last);
